10d46513f5a7974dc13f1cf38d6e2d47bc7d801c
Georges Skaf <georgesskaf@Georgess-MacBook-Pro.local>
2025-12-05 11:07:52 +0100

    modification config.py

--- config.py
+++ config.py
@@ -3,19 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TMDB_API_KEY = os.getenv('TMDB_API_KEY', '')
+TMDB_API_KEY = os.getenv('TMDB_API_KEY', 'fb9fd64171c68091b1a6eac636c7e957')
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
-
-# Dossiers
 DATA_DIR = "data"
-MODELS_DIR = "models"  # Nouveau dossier
-
-# Fichiers de données
-MOVIES_CSV = os.path.join(DATA_DIR, "movies.csv")
-EMBEDDINGS_FILE = os.path.join(DATA_DIR, "embeddings.npy")
-FAISS_INDEX_FILE = os.path.join(DATA_DIR, "faiss_index.bin")
-
-# --- CHANGEMENT ICI ---
-# Au lieu du nom en ligne, on met le chemin local
-# MODEL_NAME = 'all-MiniLM-L6-v2'  <-- L'ancien
-MODEL_PATH = os.path.join(MODELS_DIR, "film_bert") # <-- Le nouveau fine-tuné
+MODELS_DIR = "models"
+MOVIES_CSV = f"{DATA_DIR}/movies.csv"
+EMBEDDINGS_FILE = f"{DATA_DIR}/embeddings.npy"
+FAISS_INDEX_FILE = f"{DATA_DIR}/faiss_index.bin"
